4436759708957e3614a64796c96e81adffc1d3f9
mario-berube <98411521+mario-berube@users.noreply.github.com>
2023-05-30 10:35:23 -0400

    Update config.py

--- aps/config.py
+++ aps/config.py
@@ -37,7 +37,7 @@ class Config(QMainWindow):
     def add_find_file(self, box, row, title, is_dir):
         box.addWidget(QLabel(title), row, 0, 1, 2)
         text = TextBox('', readonly=False, min_size='/level0/level1/level2')
-        box.addWidget(text, row, 2, 1, 3, stretch=1)
+        box.addWidget(text, row, 2, 1, 3)
         find_file = QPushButton('Find ...')
         find_file.clicked.connect(self.set_path)
         box.addWidget(find_file, row, 6)
